e9f8ee47ea5d3d529ddb600802c3a43225dabbfb
leonardo <leo@>
2016-12-12 11:30:15 -0200

    carro

--- routes/carro.js
+++ routes/carro.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var Carro = require('../models/Usuario.js');
+var Carro = require('../models/Carro.js');
 
 /* GET /carro Listagem de carros. */
 router.get('/', function(req, res, next) {
